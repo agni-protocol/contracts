01ca5ec9edb2a2330810997877ca1bc2ef1ee323
agni <agni@agni.com>
2026-02-13 17:12:15 +0800

    rm

--- farmbooster/README.md
+++
@@ -1,1 +0,0 @@
-# Agni farm blooster
--- lm-pool/contracts/interfaces/IMasterChefV3.sol
+++
@@ -1,8 +0,0 @@
-// SPDX-License-Identifier: GPL-2.0-or-later
-pragma solidity =0.7.6;
-
-interface IMasterChefV3 {
-    function nonfungiblePositionManager() external view returns (address);
-
-    function getLatestPeriodInfo(address _pool) external view returns (uint256 cakePerSecond, uint256 endTime);
-}
